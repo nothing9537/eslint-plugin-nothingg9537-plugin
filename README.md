8bcdfa48b81921c111985e9d63eb6f4e0beaf236
Nothingg <nothing9537@gmail.com>
2023-10-21 10:12:00 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,48 @@
+# eslint-plugin-nothingg9537-plugin
+
+Eslint plugin for react-production-advanced-project
+
+## Installation
+
+You'll first need to install [ESLint](https://eslint.org/):
+
+```sh
+npm i eslint --save-dev
+```
+
+Next, install `eslint-plugin-nothingg9537-plugin`:
+
+```sh
+npm install eslint-plugin-nothingg9537-plugin --save-dev
+```
+
+## Usage
+
+Add `nothingg9537-plugin` to the plugins section of your `.eslintrc` configuration file. You can omit the `eslint-plugin-` prefix:
+
+```json
+{
+    "plugins": [
+        "nothingg9537-plugin"
+    ]
+}
+```
+
+
+Then configure the rules you want to use under the rules section.
+
+```json
+{
+    "rules": {
+        "nothingg9537-plugin/rule-name": 2
+    }
+}
+```
+
+## Rules
+
+<!-- begin auto-generated rules list -->
+TODO: Run eslint-doc-generator to generate the rules list.
+<!-- end auto-generated rules list -->
+
+
---
+++ tests/lib/rules/path-checker.js
@@ -0,0 +1,53 @@
+/**
+ * @fileoverview Path checker for production-react-advanced-project
+ * @author nothingg9537
+ */
+"use strict";
+
+//------------------------------------------------------------------------------
+// Requirements
+//------------------------------------------------------------------------------
+
+const rule = require("../../../lib/rules/path-checker"),
+  RuleTester = require("eslint").RuleTester;
+
+
+//------------------------------------------------------------------------------
+// Tests
+//------------------------------------------------------------------------------
+
+const ruleTester = new RuleTester({
+  parserOptions: {
+    ecmaVersion: 6,
+    sourceType: 'module',
+  }
+});
+ruleTester.run("path-checker", rule, {
+  valid: [
+    {
+      filename: 'C:\\Users\\Vadick\\Desktop\\projects\\production-react-advanced-project\\src\\entities\\Article',
+      code: "import { addCommentFormActions, addCommentFormReducer } from '../../model/slices/addCommentFormSlice';",
+      errors: [],
+    },
+  ],
+
+  invalid: [
+    {
+      filename: 'C:\\Users\\Vadick\\Desktop\\projects\\production-react-advanced-project\\src\\entities\\Article',
+      code: "import { addCommentFormActions, addCommentFormReducer } from 'entities/Article/model/slices/addCommentFormSlice';",
+      errors: [{
+        message: 'Bad path in local module! Please use local imports imports instead of absolute imports in module!',
+      }],
+    },
+    {
+      filename: 'C:\\Users\\Vadick\\Desktop\\projects\\production-react-advanced-project\\src\\entities\\Article',
+      code: "import { addCommentFormActions, addCommentFormReducer } from '@/entities/Article/model/slices/addCommentFormSlice';",
+      errors: [{
+        message: 'Bad path in local module! Please use local imports imports instead of absolute imports in module!',
+      }],
+      options: [{
+        alias: '@'
+      }]
+    },
+  ],
+});
